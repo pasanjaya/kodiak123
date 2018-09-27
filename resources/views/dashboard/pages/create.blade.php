06dfff7f807c72a456cd42d18031017366b16ea4
Pasan Jayawickrama <crystalcbuddy@localhost.localdomain>
2018-09-27 12:30:26 +0530

    add card to update offer section

--- resources/views/dashboard/pages/create.blade.php
+++ resources/views/dashboard/pages/create.blade.php
@@ -4,14 +4,14 @@
     <div class="container">
         <div class="card border-info" style="width: 75rem;">
             <h2 class="card-header mb-5">Create Advertisement</h2>
-            <div class="card-body">
+            <div class="card-body container">
             {!! Form::open(['action' => 'AdvertisementController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                <div class="form-group">
+                <div class="form-group col-10">
                     {{Form::label('title', 'Title *')}}
                     {{Form::text('title', '', ['class'=> 'form-control', 'placeholder' => 'Title'])}}
                     
                 </div>
-                <div class="form-group">
+                <div class="form-group col-10">
                     {{Form::label('subtitle', 'Subtitle')}}
                     {{Form::text('subtitle', '', ['class'=> 'form-control', 'placeholder' => 'Subtitle'])}}
                 </div>
@@ -23,35 +23,38 @@
                     {{Form::label('regprice', 'Regular Price')}}
                     {{Form::text('regprice', '', ['class'=> 'form-control', 'placeholder' => 'Regular Price'])}}
                 </div> --}}
-                <div class="form-group">
+                <div class="form-group col-10">
                     {{Form::label('description', 'Description')}}
                     {{Form::textarea('description', '', ['id'=> 'article-ckeditor','class'=> 'form-control', 'placeholder' => 'Description'])}}
                 </div>
-                <div class="form-group">
+                <div class="form-group col-10">
                     {{Form::label('tags', 'Tags *')}}
                     {{Form::text('tags', '', ['class'=> 'form-control', 'placeholder' => 'Tags'])}}
                 </div>
-                <div class="form-group">
+                <div class="form-group col-10">
                     {{Form::label('image', 'Offer ad Image *')}}
                     {{Form::file('image', ['class'=> 'form-control-file', 'placeholder' => 'Image'])}}
                 </div>
-                
-                <div class="form-group">
+
+                <div class="form-group col-6">
                     {{Form::label('start_date', 'Offer Start On * (mm/dd/yyyy)')}}
                     {{Form::date('start_date', \Carbon\Carbon::now()->toDateString(), ['class'=> 'form-control'])}}
                 </div>
-                <div class="form-group">
+                <div class="form-group col-6">
                     {{Form::label('end_date', 'Offer End On * (mm/dd/yyyy)')}}
                     {{Form::date('end_date', \Carbon\Carbon::tomorrow()->toDateString(), ['class'=> 'form-control'])}}
                 </div>
+
                 {{-- <div class="form-group">
                     {{Form::label('link', 'Offer Catch Link')}}
                     {{Form::text('link', '', ['class'=> 'form-control', 'placeholder' => 'Official link of offer'])}}
                 </div> --}}
-                {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+                <div class="form-group col-3 mt-5">
+                    {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+                </div>
                 <br><br>
             {!! Form::close() !!}
-        </div>
+            </div>
         </div>
     </div>
         </main>
--- resources/views/dashboard/pages/edit.blade.php
+++ resources/views/dashboard/pages/edit.blade.php
@@ -1,45 +1,51 @@
 @extends('layouts.appad')
 
 @section('content')
-
-            <h1 class="mb-5">Update Advertisement</h1>
-
-            {!! Form::open(['action' => ['AdvertisementController@update', $ads->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
-                <div class="form-group">
-                    {{Form::label('title', 'Title *')}}
-                    {{Form::text('title', $ads->title, ['class'=> 'form-control', 'placeholder' => 'Title'])}}
-                    
+            <div class="container">
+                <div class="card border-info" style="width: 75rem;">
+                    <h2 class="card-header mb-5">Update Advertisement</h2>
+                    <div class="card-body container">
+                    {!! Form::open(['action' => ['AdvertisementController@update', $ads->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+                        <div class="form-group col-10">
+                            {{Form::label('title', 'Title *')}}
+                            {{Form::text('title', $ads->title, ['class'=> 'form-control', 'placeholder' => 'Title'])}}
+                            
+                        </div>
+                        <div class="form-group col-10">
+                            {{Form::label('subtitle', 'Subtitle')}}
+                            {{Form::text('subtitle', $ads->subtitle, ['class'=> 'form-control', 'placeholder' => 'Subtitle'])}}
+                        </div>
+                        <div class="form-group col-10">
+                            {{Form::label('description', 'Description')}}
+                            {{Form::textarea('description', $ads->description, ['id'=> 'article-ckeditor','class'=> 'form-control', 'placeholder' => 'Description'])}}
+                        </div>
+                        <div class="form-group col-10">
+                            {{Form::label('tags', 'Tags *')}}
+                            {{Form::text('tags', $ads->tags, ['class'=> 'form-control', 'placeholder' => 'Tags'])}}
+                        </div>
+                        <div class="form-group col-10">
+                            {{Form::label('image', 'Offer ad Image *')}}
+                            {{Form::file('image', ['class'=> 'form-control-file', 'placeholder' => 'Image'])}}
+                        </div>
+                        
+                        <div class="form-group col-6">
+                            {{Form::label('start_date', 'Offer Start On * (mm/dd/yyyy)')}}
+                            {{Form::date('start_date', $ads->start_date, ['class'=> 'form-control'])}}
+                        </div>
+                        <div class="form-group col-6">
+                            {{Form::label('end_date', 'Offer End On * (mm/dd/yyyy)')}}
+                            {{Form::date('end_date', $ads->end_date, ['class'=> 'form-control'])}}
+                        </div>
+                        <div class="form-group col-3 mt-5">
+                            {{Form::hidden('_method', 'PUT')}}
+                            {{Form::submit('Update', ['class' => 'btn btn-primary'])}}
+                        </div>
+                    {!! Form::close() !!}
+                    </div>
                 </div>
-                <div class="form-group">
-                    {{Form::label('subtitle', 'Subtitle')}}
-                    {{Form::text('subtitle', $ads->subtitle, ['class'=> 'form-control', 'placeholder' => 'Subtitle'])}}
-                </div>
-                <div class="form-group">
-                    {{Form::label('description', 'Description')}}
-                    {{Form::textarea('description', $ads->description, ['id'=> 'article-ckeditor','class'=> 'form-control', 'placeholder' => 'Description'])}}
-                </div>
-                <div class="form-group">
-                    {{Form::label('tags', 'Tags *')}}
-                    {{Form::text('tags', $ads->tags, ['class'=> 'form-control', 'placeholder' => 'Tags'])}}
-                </div>
-                <div class="form-group">
-                    {{Form::label('image', 'Offer ad Image *')}}
-                    {{Form::file('image', ['class'=> 'form-control-file', 'placeholder' => 'Image'])}}
-                </div>
-                
-                <div class="form-group">
-                    {{Form::label('start_date', 'Offer Start On * (mm/dd/yyyy)')}}
-                    {{Form::date('start_date', $ads->start_date, ['class'=> 'form-control'])}}
-                </div>
-                <div class="form-group">
-                    {{Form::label('end_date', 'Offer End On * (mm/dd/yyyy)')}}
-                    {{Form::date('end_date', $ads->end_date, ['class'=> 'form-control'])}}
-                </div>
-                {{Form::hidden('_method', 'PUT')}}
-                {{Form::submit('Update', ['class' => 'btn btn-primary'])}}
-            {!! Form::close() !!}
-            </main>
-        </div>
-    </div>   
+            </div>
+        </main>
+    </div>
+</div>   
 
 @endsection
