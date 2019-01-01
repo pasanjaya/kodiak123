455fb8ac909cb3d0586f83d58846a3ed6fb5e331
pasanjaya <bhadrajeepjaya@gmail.com>
2019-01-01 12:29:48 +0530

    updated profile

--- resources/views/dashboard/pages/profile.blade.php
+++ resources/views/dashboard/pages/profile.blade.php
@@ -6,15 +6,38 @@
     <div class="h2 text-info">Edit Profile</div><hr>
     <div class="row">
         <div class="col-3">
-            <div class="row justify-content-center mb-4">
-                <img src="/svg/man.svg" alt="im_advertiser" class="rounded-circle border border-dark mr-2" style="width:120px; height:120px;">
-            </div>
-            <div class="row text-center justify-content-center">
-                <form>
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Upload a different image</label>
-                        <input type="file" class="form-control-file border" id="exampleFormControlFile1">
+            <div class="card border-0" style="height:370px;">
+                <div class="card-body">
+                    <div class="row justify-content-center mb-4">
+                        <img src="/svg/man.svg" alt="im_advertiser" class="rounded-circle border border-dark mr-2" style="width:120px; height:120px;">
                     </div>
+                    {{-- <div class="row text-center justify-content-center">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleFormControlFile1">Upload a different image</label>
+                                <input type="file" class="form-control-file border" id="exampleFormControlFile1">
+                            </div>
+                    </div> --}}
+                    <form action="">
+                        <div class="form-group">
+                            <div class="row text-center justify-content-center">
+                                <label for="exampleFormControlFile1" class="">Upload a different image</label>
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Image</span>
+                                </div>
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="inputGroupFile01">
+                                    <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row text-center justify-content-center">
+                            <button type="submit" class="btn btn-primary">Change Image</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
         
@@ -79,12 +102,6 @@
                                     </div>
                                 </div>
                                 <div class="form-group row">
-                                    <label for="staticEmail" class="col-sm-2 col-form-label">Registerd Name</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
-                                    </div>
-                                </div>
-                                <div class="form-group row">
                                     <label for="inputState" class="col-sm-2 col-form-label">Category</label>
                                     <div class="col-sm-10">
                                         <select id="inputState" class="form-control">
@@ -93,7 +110,78 @@
                                         </select>
                                     </div>
                                 </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="form-group row">
+                                    <label for="inputState" class="col-sm-2 col-form-label">Sub Category</label>
+                                    <div class="col-sm-10">
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose...</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">About Company</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Street Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputState" class="col-sm-2 col-form-label">City</label>
+                                    <div class="col-sm-10">
+                                        <select id="inputState" class="form-control">
+                                            <option selected>Choose...</option>
+                                            <option>...</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Telephone No.</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Website</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Business Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Inquiry Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="staticEmail" value="{{auth()->user()->name}}">
+                                    </div>
+                                </div>
+                                {{-- <div class="form-group row">
+                                    <label for="staticEmail" class="col-sm-2 col-form-label">Preferred Sections</label>
+                                    <div class="form-check col-sm-10">
+                                        <div class="row col-sm-10">
+                                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                            <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+                                        </div>
+                                        <div class="row col-sm-10">
+                                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                            <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+                                        </div>
+                                        <div class="row col-sm-10">
+                                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                            <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+                                        </div>
+                                    </div>
+                                </div> --}}
+                                <button type="submit" class="btn btn-primary">Save Changes</button>
                             </form>
                         </div>
                         <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
