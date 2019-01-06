233269cbb2e41079303c81daff5d02aac056c5c7
Harshanee <40499077+Harshanee@users.noreply.github.com>
2019-01-06 23:16:04 +0530

    mailconfig

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'kodiakmailservice@mail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'kodiakmailservice@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Kodiak Team'),
     ],
 
