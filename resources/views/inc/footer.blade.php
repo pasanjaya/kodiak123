25d2d1a9b9c37dbd30e8216f213d0a95459646b5
Harshanee <40499077+Harshanee@users.noreply.github.com>
2019-01-10 09:39:15 +0530

    comments

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -96,13 +96,11 @@
 
         <div class="p-t-40">
             <p class="stext-107 cl6 txt-center">
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                
                 Copyright &copy;
                 <script>document.write(new Date().getFullYear());</script> All rights reserved | This is made and
                 developed by KODIAK
-                <!--i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a-->
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
+                
             </p>
         </div>
     </div>
